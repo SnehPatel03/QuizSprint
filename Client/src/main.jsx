20e0d8aba3a7808d31d6da95ecdd89b33b94b002
Sneh Patel <snehpatel@Snehs-MacBook-Air.local>
2026-01-26 23:52:51 +0530

    Changed for configration

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
     <ToastContainer position="top-right" autoClose={3000} />
-  </BrowserRouter>,
+  </HashRouter>,
 );
